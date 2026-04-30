955ffa8fa78cd3b6ef6530f428608836c364f888
Giorgi <157357409+Gl1tCh1121@users.noreply.github.com>
2026-04-30 19:02:10 +0400

    HW4

---
+++ HW4.py
@@ -0,0 +1,67 @@
+# N1
+
+num = int(input("Write positive whole number: "))
+
+while num > 0:
+    print(num)
+    num -= 1
+
+print("text")
+
+
+# N2
+
+summ = 0
+while True:
+    numbers = int(input("Write whole numbers: "))
+    summ += numbers
+    if numbers == 0:
+        break 
+
+print(summ)
+
+# N3
+
+sec_num = 11
+
+while True:
+    guess_num = int(input("Guess the number: "))
+    if guess_num > sec_num:
+        print("Too high")
+    elif guess_num < sec_num:
+        print("Too low")
+    else:
+        print("Correct")
+        break
+
+
+
+# N4 
+
+list = ["a", "e", "i","o", "u"]
+filterd_text = ""
+text = input("Text: ")
+length = len(text)
+
+
+while length != 0:
+    length -= 1
+    if text[length] not in list:
+        filterd_text += text[length]
+    
+print(filterd_text)
+
+
+# N5
+
+for i in range(0, 9):
+    print(i)
+
+for i in range(5, 15):
+    print(i)
+
+for i in range(0, 20, 2):
+    print(i)
+
+for i in range(10, 1, -1):
+    print(i)
